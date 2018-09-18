2c3530b2b70db75a562247bb39269a8090914c9f
Luke Voyer <lvoye451@live.kutztown.edu>
2018-09-17 21:46:46 -0400

    Added comments

--- CompoundInterest/src/CompoundInterest.java
+++ CompoundInterest/src/CompoundInterest.java
@@ -3,23 +3,33 @@ import java.util.Scanner;
 
 public class CompoundInterest {
 	public static void main(String[] args) {
+		//Scanner var for user input
 		Scanner userInput = new Scanner(System.in);
 		
+		//Enter Principle
 		System.out.println("Enter principle amount: ($___)");
         double principle = userInput.nextDouble();
+        
+        //Enter Rate
         System.out.println("Enter rate at which the principle is to be compounded: (___%)");
         double rate = userInput.nextDouble();
+        
+        //Enter Time in Years
         System.out.println("Enter the time in years the principle is to be compounded: (___)");
         double time = userInput.nextDouble();
         
+        //Close input
         userInput.close();
         
-        /* Calculate compound interest */
+        //Calculate compound interest
         double CI = principle * (Math.pow((1 + rate / 100), time));
-         
+        
+        //Print amount with formatted price
         System.out.println("Your compound interest amount is:" + '\n' + formatPrice(CI));
 	}
 	
+	
+	//Added function to format price
 	private static String formatPrice(final double priceAsDouble) 
 	{
 	    NumberFormat formatter = NumberFormat.getCurrencyInstance();
